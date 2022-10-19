685deae1b84c6629be501d372124c77e28fc79e3
David Ebukali <debukali@gmail.com>
2022-10-19 10:51:13 +0300

    Install fontawesome icons

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue';
 import './index.css';
 import router from './router';
 import cache from './utils/cache';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).use(router).use(cache, { expires: 5 }).mount('#app');
+library.add(faCopy, faCheck);
+
+createApp(App).use(router).use(cache, { expires: 5 }).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
