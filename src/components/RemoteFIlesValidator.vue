eba4ad968315c0fec0c039f6796527200d2c89d6
Bibiana Cristofol Amat <bcristofol@gmail.com>
2024-11-19 12:55:18 +0100

    src: components: RemoteFilesValidator: fix spelling error

--- src/components/RemoteFIlesValidator.vue
+++ src/components/RemoteFIlesValidator.vue
@@ -75,7 +75,7 @@
   <div class="-m-2.5 flex flex-wrap pt-4">
     <CardiB heading="Step 1" class="w-[300px]" :class="{ 'border-t-iati-blue': activeStep !== 1 }">
       <label for="url" class="mb-4 text-center">
-        Add a web address (URL) of your IATI XML file. You can add multiple files by seperating them with |
+        Add a web address (URL) of your IATI XML file. You can add multiple files by separating them with |
       </label>
       <input
         id="url"
