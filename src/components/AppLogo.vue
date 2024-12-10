0de1f374a4132345703f048557130dd323d059b5
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-12-10 16:49:18 +0200

    refactor: split header into smaller components

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,7 +1,5 @@
 <template>
-  <a
-    class="relative m-0 block h-10 w-44 bg-logo-colour bg-contain bg-no-repeat sm:mb-auto sm:h-14 sm:w-64"
-    href="https://iatistandard.org"
-    title="To the main IATI Standard website"
-  ></a>
+  <a href="https://iatistandard.org/" aria-label="Go to IATI homepage">
+    <img class="iati-header__logo" alt="" src="../assets/svg/logo-colour.svg" />
+  </a>
 </template>
