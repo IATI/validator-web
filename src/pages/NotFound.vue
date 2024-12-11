066d9b13c31c18a6274d529254e9d68ce80cf93a
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-12-11 11:51:31 +0200

    feat: remove custom styling from not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,4 @@
 <template>
-  <h1 class="text-4xl">Page not found</h1>
-  <p class="pt-4 text-lg">Sorry, we couldn't find that page</p>
+  <h1>Page not found</h1>
+  <p>Sorry, we couldn't find that page.</p>
 </template>
